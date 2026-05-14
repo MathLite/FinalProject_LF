70c94878d0c74a80b5ed60c161aac91618d29e44
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-13 21:14:23 -0500

    funciones matemáticas e implementación de AssignNode

--- syntax_parser.py
+++ syntax_parser.py
@@ -118,6 +118,11 @@ class Parser:
 
         if self.check("RETURN"):
             return self.parse_return_stmt()
+        
+        if self.check("ID"):
+            next_token_index = self.pos + 1
+            if next_token_index < len(self.tokens) and self.tokens[next_token_index].type == "ASSIGN":
+                return self.parse_assign_stmt()
 
         return self.parse_expr_stmt()
 
@@ -292,6 +297,14 @@ class Parser:
 
         return ExprStmtNode(expr)
 
+    def parse_assign_stmt(self):
+
+        name_token = self.advance()
+        self.consume("ASSIGN", "Se esperaba '='")
+        value_expr = self.parse_expression()
+
+        return AssignNode(name_token.lexeme, value_expr)
+
     # =========================
     # EXPRESIONES
     # =========================
@@ -452,7 +465,7 @@ class Parser:
         if self.match("FALSE"):
             return BoolNode(False)
 
-        if self.match("ID"):
+        if self.match("ID", "SIN", "COS", "TAN", "SQRT", "LOG", "ABS", "FLOOR", "CEIL"):
 
             name = self.previous().lexeme
 
