bf81a800152931f0dca90b02697dae9f424b8c99
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-29 20:37:59 -0500

    Configuracion para desplegar con Render

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,26 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
 def initialize_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate(
-            "firebase_credentials/firebase_key.json"
-        )
+        firebase_key_env = os.environ.get("FIREBASE_KEY_JSON")
+        
+        if firebase_key_env:
+            try:
+                cred_dict = json.loads(firebase_key_env)
+                cred = credentials.Certificate(cred_dict)
+            except Exception as e:
+                cred = credentials.Certificate(
+                    "firebase_credentials/firebase_key.json"
+                )
+        else:
+            cred = credentials.Certificate(
+                "firebase_credentials/firebase_key.json"
+            )
+            
         firebase_admin.initialize_app(cred)
 
     return firestore.client()
